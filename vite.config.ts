531c60c988e0e35235689272022179d73abf4f6b
rosa-developer <121453025+rosa-developer@users.noreply.github.com>
2025-03-25 17:49:41 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "./src/new-design.scss";` },
+    },
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
